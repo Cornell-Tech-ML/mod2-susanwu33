2d43e3f260a61c7340714a9708a1cce9ff3ff9bd
susanwu33 <1063186256@qq.com>
2024-10-22 15:11:55 -0400

    Finally solved and passed all

--- minitorch/__init__.py
+++ minitorch/__init__.py
@@ -1,3 +1,5 @@
+"""The minitorch library provides core functionalities, including tensor operations, autodiff capabilities, and various utilities."""
+
 from .testing import MathTest, MathTestVariable  # type: ignore # noqa: F401,F403
 from .tensor_data import *  # noqa: F401,F403
 from .tensor import *  # noqa: F401,F403
